141de6a1a6631fc727391f484a13cbe31da9cf90
Valeria Melnyk <valeria1melnyk@gmail.com>
2025-03-18 23:15:13 +0200

    implementation patterns

--- Main.java
+++ Main.java
@@ -6,6 +6,14 @@ import adapter.Communication;
 import adapter.VampireCommunication;
 import adapter.WizardCommunication;
 import bridge.*;
+import decorator.BasicSpeed;
+import decorator.ShadowMode;
+import facade.ClanFacade;
+import flyweight.VampireFactory;
+import flyweight.VampireType;
+import proxy.ProxyVampireCommander;
+import proxy.RealVampireCommander;
+import proxy.VampireCommander;
 
 public class Main {
     public static void main(String[] args) {
@@ -30,5 +38,25 @@ public class Main {
         clan.addVampire(lord2);
 
         clan.showName();
+
+        decorator.VampireAbility speed1 = new BasicSpeed();
+        decorator.VampireAbility shadowSpeed = new ShadowMode(speed1);
+
+        shadowSpeed.useAbility();
+
+        ClanFacade clanFacade = new ClanFacade();
+        clanFacade.manageClan();
+
+        VampireType minion1 = VampireFactory.getVampireType("Слуга");
+        VampireType minion2 = VampireFactory.getVampireType("Слуга");
+        minion1.showType();
+        minion2.showType();
+        System.out.println("Перевірка пам'яті: однакові об'єкти -> " + (minion1 == minion2));
+
+
+        RealVampireCommander realCommander = new RealVampireCommander();
+        VampireCommander proxyCommander = new ProxyVampireCommander(realCommander);
+        proxyCommander.giveOrder("Знайти нову жертву");
+
     }
 }
